8a88607d7dbb1b48482b2efb6905e684927fcbaa
Yang Yu <75100383+MrBetta520@users.noreply.github.com>
2022-01-25 22:05:00 -0500

    Add files via upload
    A school is trying to take an annual photo of all the students. The students are asked to stand in a single file line in non-decreasing order by height. Let this ordering be represented by the integer array expected where expected[i] is the expected height of the ith student in line.
    
    You are given an integer array heights representing the current order that the students are standing in. Each heights[i] is the height of the ith student in line (0-indexed).
    
    Return the number of indices where heights[i] != expected[i].

---
+++ HeightChecker/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeightChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static int HeightChecker(int[] heights)
+        {
+            int[] expected = new int[heights.Length];
+
+            for (int i = 0; i < expected.Length; i++)
+            {
+                foreach(int h in heights)
+                {
+                    expected[i] = h;
+                }
+            }
+
+            Array.Sort(expected);
+
+            int n = 0;
+
+            for (int i = 0; i < heights.Length; i++)
+            {
+                if(heights[i] != expected[i])
+                {
+                    n++;
+                }
+            }
+
+            return n;
+        }
+    }
+}
