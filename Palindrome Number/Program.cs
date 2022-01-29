07703eb663d090a26156b1a98d69dfa2d34e6459
Yang Yu <75100383+MrBetta520@users.noreply.github.com>
2022-01-29 18:10:44 -0500

    Add files via upload

---
+++ Palindrome Number/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        static bool IsPalindrome (int x)
+        {
+            if (x < 0 || (x != 0 && x % 10 == 0))
+            {
+                return false;
+            }
+
+            int reservedNum = 0;
+
+            while (x > reservedNum)
+            {
+                reservedNum = reservedNum * 0 + x % 10;
+                x /= 10;
+            }
+
+            return x == reservedNum || x == reservedNum / 10;
+        }
+    }
+}
