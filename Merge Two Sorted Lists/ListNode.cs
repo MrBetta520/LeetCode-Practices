7704973e2a2c87c855d443f921e653e4f165a1bb
Yang Yu <75100383+MrBetta520@users.noreply.github.com>
2022-01-30 22:07:45 -0500

    Update ListNode.cs

--- Merge Two Sorted Lists/ListNode.cs
+++ Merge Two Sorted Lists/ListNode.cs
@@ -8,5 +8,14 @@ namespace Merge_Two_Sorted_Lists
 {
     class ListNode
     {
+        public int val { get; set;}
+        
+        public ListNode next { get; set;}
+        
+        public ListNode (int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
     }
 }
