28bc1966494f969130ce0d9390fc4f3bdae34e97
Yang Yu <75100383+MrBetta520@users.noreply.github.com>
2022-01-30 22:05:19 -0500

    Add files via upload

--- Merge Two Sorted Lists/ListNode.cs
+++ Merge Two Sorted Lists/ListNode.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LinkedList
+namespace Merge_Two_Sorted_Lists
 {
     class ListNode
     {
--- Merge Two Sorted Lists/Program.cs
+++ Merge Two Sorted Lists/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LinkedList
+namespace Merge_Two_Sorted_Lists
 {
     class Program
     {
@@ -12,30 +12,31 @@ namespace LinkedList
         {
             
         }
-        static ListNode MergeTwoLists(ListNode l1, ListNode l2)
+
+        static ListNode MergeTwoLists (ListNode l1, ListNode l2)
         {
             ListNode head = new ListNode();
             ListNode tail = head;
 
-            while (l1 != null & l2 != null)
+            while (l1 != null && l2 != null)
             {
-                if (l1.val <= l2.val)
+                if(l1.val <= l2.val)
                 {
-                    tail.next = l1;
+                    tail = l1;
                     l1 = l1.next;
                 }
                 else
                 {
-                    tail.next = l2;
+                    tail = l2;
                     l2 = l2.next;
                 }
+
                 tail = tail.next;
             }
-            tail.next = l1 == null ? l2 : l2;
+
+            tail.next = l1 == null ? l2 : l1;
 
             return head.next;
         }
-
-        
     }
 }
