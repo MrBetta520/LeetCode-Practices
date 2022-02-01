dd686737b881f8772624dc1bde2e1c1306bd2f4f
Yang Yu <75100383+MrBetta520@users.noreply.github.com>
2022-02-01 10:07:35 -0500

    Add files via upload

---
+++ Reverse Words in a String/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_Words_in_a_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = ReverseWords(" a good  example");
+
+            Console.WriteLine(s);
+            Console.ReadLine();
+        }
+
+        static string ReverseWords(string s)
+        {
+            string[] sa = s.Trim().Split(' ');
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = sa.Length - 1; i >= 0; i--)
+            {
+                if (sa[i] != "")
+                {
+                    sb.Append(sa[i]).Append(" ");
+                }
+            }
+
+            return sb.ToString().Trim();
+        }
+}
