1f600f5871ca9df8f270d123ab41b510ad1a99dc
Yang Yu <75100383+MrBetta520@users.noreply.github.com>
2022-03-22 18:55:36 -0400

    Add files via upload

---
+++ Find_Pivot_Index/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Find_Pivot_Index
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int PivotIndex(int[] nums)
+        {
+            int sum = 0;
+            int leftSum = 0;
+
+
+            foreach (int n in nums)
+            {
+                sum += n;
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (sum - nums[i] - leftSum == leftSum)
+                {
+                    return i;
+                }
+                leftSum += nums[i];
+            }
+
+            return -1;
+        }
+    }
+}
