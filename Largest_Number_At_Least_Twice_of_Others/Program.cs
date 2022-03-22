692c4eb17cab4d5289b344310c129523b96bf005
Yang Yu <75100383+MrBetta520@users.noreply.github.com>
2022-03-22 18:45:27 -0400

    Add files via upload

---
+++ Largest_Number_At_Least_Twice_of_Others/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Largest_Number_At_Least_Twice_of_Others
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 3, 6, 1, 0 };
+            Console.WriteLine(DominantIndex(a));
+
+            Console.ReadLine();
+        }
+
+        public static int DominantIndex(int[] nums)
+        {
+            if(nums.Length == 1)
+            {
+                return 0;
+            }
+
+            int max = nums.Max();
+            int index = 0;
+
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if(nums[i] == max)
+                {
+                    index = i;
+                }
+                else if(nums[i] != max && (nums[i] * 2 < max))
+                {
+                    index = -1;
+                }
+            }
+
+            return index;
+        }
+    }
+}
